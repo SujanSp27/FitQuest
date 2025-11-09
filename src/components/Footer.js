11ee947204e3683430c92e991cbc782e4f9dba5c
Sujan Poojary <poojarysujan77@gmail.com>
2025-11-09 12:09:07 +0530

    feat: initial layout setup with routing and base components

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      <p>© 2025 FitQuest | All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ExerciseDetail = () => {
+  return <div>Exercise Detail Page</div>;
+};
+
+export default ExerciseDetail;
+
